5df8a7eef103ff2971b6a7eb0fe49ac74d0ad276
GanghyeonSeo <sgh000109@gmail.com>
2021-05-13 20:36:28 +0900

    dict_cursor 추가

--- methods/challenge.py
+++ methods/challenge.py
@@ -4,7 +4,7 @@ from constants import messages
 
 
 def get_challenge(challenge_id: int):
-    sql = MySQL()
+    sql = MySQL(dict_cursor=True)
 
     result = sql.query('SELECT id, HEX(submitter), HEX(category), name, auth_way, auth_day, auth_count_in_day, '
                        'start_at, end_at, cost, description FROM challenge WHERE id=%s', (challenge_id,))
